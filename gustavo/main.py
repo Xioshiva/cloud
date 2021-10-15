80ff127c67c2a61d53b398ed976e92a6add6a914
gustavo.pertuzat <gustavo.pertuzat@hes-so.ch>
2021-10-15 15:16:08 +0200

    instance from image generation implemented

--- gustavo/main.py
+++ gustavo/main.py
@@ -6,26 +6,30 @@ def main():
 
     conn = openstack.connect(cloud='openstack')
 
-    front = create_server(conn, "frontend", "m1.small", "private", "gus", "frontend")
+    db = conn.create_server(name='db', 
+                            image='a5f12d10-9ef7-43b7-bf59-b577f1302029', 
+                            auto_ip=False,
+                            flavor='m1.small', 
+                            wait=True, 
+                            security_groups='database', 
+                            key_name='gus',
+                            userdata="""#!/bin/bash \n cd ~/db/ && node server.js""")
 
-    back = create_server(conn, "backend", "m1.small", "private", "gus", "backend")
+    back = conn.create_server(name='back', 
+                              image='b9eeb84c-56ec-4388-b0a2-abd85c5ef613', 
+                              flavor='m1.small', 
+                              wait=True, 
+                              security_groups='backend', 
+                              key_name='gus',
+                              userdata="""#!/bin/bash \n cd ~/backend/ && sed -i '25s/.*/var dbLink = \"http:\/\/"""+ db.ipv4_address+""":8081\";/' server.js && node server.js""")
 
-    db = create_server(conn, "database", "m1.small", "private", "gus", "database")
-
-
-def create_server(conn, image_name, flavor_name, network_name, keypair_name, server_name):
-    print("Create Server: ")
-    
-    image = conn.compute.find_image(image_name)
-    flavor = conn.compute.find_flavor(flavor_name)
-    network = conn.compute.find_network(network_name)
-    keypair = conn.compute.find_keypair(keypair_name)
-
-    server = conn.compute.create_server(
-        name=server_name, image_id=image.id, flavor_id=flavor.id, 
-        networks=[{"uuid": network.id}], key_name=keypair.name)
-    
-    server = conn.compute.wait_for_server(server)
+    front = conn.create_server(name='front', 
+                              image='1504a752-ad5b-4af0-a03a-f0cc68e92f82', 
+                              flavor='m1.small', 
+                              wait=True, 
+                              security_groups='frontend', 
+                              key_name='gus',
+                              userdata= """#!/bin/bash \n cd ~/frontend && sed -i '2s/.*/let address = \""""+back.ipv4_address+"""\";/'  classements/classements.js && python3 -m http.server""")
 
 if __name__ == "__main__":
     main()
