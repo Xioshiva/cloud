2685828e26720677221f113426005260c6a29e0f
gustavo.pertuzat <gustavo.pertuzat@hes-so.ch>
2021-10-18 23:43:29 +0200

    final commit

--- gustavo/main.py
+++ gustavo/main.py
@@ -5,6 +5,7 @@ openstack.enable_logging(debug=True, stream=sys.stdout)
 
 conn = openstack.connect(cloud='openstack')
 
+# Create db instance
 db = conn.create_server(name='db', 
                         image='a5f12d10-9ef7-43b7-bf59-b577f1302029', 
                         auto_ip=False,
@@ -12,20 +13,45 @@ db = conn.create_server(name='db',
                         wait=True, 
                         security_groups='database', 
                         key_name='gus',
-                        userdata="""#!/bin/bash \n cd ~/db/ && node server.js""")
+                        userdata="""
+                        #cloud-config
+                        runcmd:
+                            - cd /home/ubuntu/db/
+                            - node server.js
+                        """)
 
+db_active = conn.compute.wait_for_server(db) # wait until active
+db_ip = conn.get_server_private_ip(db_active)
+
+# Create backend instance
 back = conn.create_server(name='back', 
                           image='b9eeb84c-56ec-4388-b0a2-abd85c5ef613', 
                           flavor='m1.small', 
                           wait=True, 
                           security_groups='backend', 
                           key_name='gus',
-                          userdata="""#!/bin/bash \n cd ~/backend/ && sed -i '25s/.*/var dbLink = \"http:\/\/"""+ db.ipv4_address+""":8081\";/' server.js && node server.js""")
+                          userdata="""
+                          #cloud-config
+                          runcmd:
+                            - cd /home/ubuntu/backend/
+                            - sed -i '25s/.*/var dbLink = \"http:\/\/"""+db_ip+""":8081\";/' server.js
+                            - node server.js
+                          """)
 
+back_active = conn.compute.wait_for_server(back) # wait until active
+back_ip = conn.get_server_public_ip(back_active)
+
+# Create frontend instance
 front = conn.create_server(name='front', 
                           image='1504a752-ad5b-4af0-a03a-f0cc68e92f82', 
                           flavor='m1.small', 
                           wait=True, 
                           security_groups='frontend', 
                           key_name='gus',
-                          userdata= """#!/bin/bash \n cd ~/frontend && sed -i '2s/.*/let address = \""""+back.ipv4_address+"""\";/'  classements/classements.js && python3 -m http.server""")
+                          userdata= """
+                          #cloud-config
+                          runcmd:
+                            - cd /home/ubuntu/frontend
+                            - sed -i '2s/.*/let address = \""""+back_ip+"""\";/'  classements/classements.js
+                            - python3 -m http.server
+                          """)
