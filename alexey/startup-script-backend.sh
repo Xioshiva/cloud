a8fef882e546d05f6aba3001e4b0f22439764d3a
alexey.paulot <alexey.paulot@etu.hesge.ch>
2021-10-19 18:07:52 +0200

    Readme ajoute

--- alexey/startup-script-backend.sh
+++ alexey/startup-script-backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/kekusmax/home
-sed -i '25s/.*/var dbLink = "http:\/\/34.71.178.28:8081";/' server.js
+sed -i '25s/.*/var dbLink = "http:\/\/34.70.59.67:8081";/' server.js
 node server.js
--- alexey/startup-script-frontend.sh
+++ alexey/startup-script-frontend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/kekusmax/frontend/frontend/classements
-sed -i '2s/.*/let address = "35.225.209.68";/' classements.js
+sed -i '2s/.*/let address = "130.211.201.255";/' classements.js
 cd ..
 python3 -m http.server
